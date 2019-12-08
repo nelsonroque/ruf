7d9298207b7a800dd537f53eb1e8c6e46c5b1b58
Nelson Roque <nelsonroquejr@gmail.com>
2019-12-07 21:14:01 -0500

    up

--- R/lmm_pie.R
+++ R/lmm_pie.R
@@ -1,26 +1,26 @@
 #' ruf
 
-#' @name lmm2pie
+#' @name lmm_pie
 #' @param fit class: model fit output object
 #' @param pkg class: string
 #' @keywords ecological momentary assessment data, intensive longitudinal data
 #' @import tidyverse
 #' @import nlme
 #' @examples
-#' lmm2pie(fit, pkg="lme4")
+#' lmm_pie(fit, pkg="lme4")
 #' @export
-lmm2pie <- function(fit, pkg="lme4") {
+lmm_pie <- function(fit, pkg="lme4") {
   
-  pie.df <- tibble::as.tibble(lme4::VarCorr(fit)) %>%
+  pie_df <- tibble::as.tibble(lme4::VarCorr(fit)) %>%
     dplyr::mutate(prop_var = (vcov/sum(vcov)),
                   perc_var = (vcov/sum(vcov))*100,
                   label = paste0(grp,"*",var1,"*",var2))
   
-  pie.sum <- pie.df %>% 
+  pie_sum <- pie_df %>% 
     dplyr::group_by(grp) %>% 
     dplyr::summarise(prop_var = sum(prop_var))
   
-  pie.plot <- ggplot2::ggplot(pie.sum, aes(x="", y=prop_var, fill=grp)) +
+  pie_plot <- ggplot2::ggplot(pie_sum, aes(x="", y=prop_var, fill=grp)) +
     geom_bar(width = 1, stat = "identity") +
     coord_polar("y",start=0) +
     theme_minimal() +
@@ -28,7 +28,7 @@ lmm2pie <- function(fit, pkg="lme4") {
     ylab("Proportion of Variance") +
     scale_fill_viridis_d()
 
-  return(list(variance_table = pie.df, 
-              summary = pie.sum,
-              plot = pie.plot))
+  return(list(variance_table = pie_df, 
+              summary = pie_sum,
+              plot = pie_plot))
 }
