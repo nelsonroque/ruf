737b3c1a8d6d2a6f15d3936c61a065d0fb6674c9
Nelson Roque <nelsonroquejr@gmail.com>
2020-01-31 15:32:48 -0500

    up

--- R/qprint.R
+++ R/qprint.R
@@ -4,7 +4,7 @@ qprint <- function(., delim=" : ") {
   if(is_data_frame_tibble(.)) {
     stop("Printing a data.frame to the console is memory intensive. This function is intended to be used with integers, strings, and other small objects. Use lprint() for larger objects.")
   } else {
-    print(paste0("object name: ", deparse(substitute(.))))
-    print(paste0("object value: ", .))
+    print(paste0("object name", delim, deparse(substitute(.))))
+    print(paste0("object value", delim, .))
   }
 }
